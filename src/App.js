4a2c8be6720c277fb9ab8d094f898dbc2626edea
naoto N <n@MacBook-Pro-N.local>
2020-10-22 23:22:34 +0900

    :sparkles: larn jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 function App() {
-  return <div><h1>Hello, world!</h1></div>;
+  return (
+    <div>
+      <label htmlFor="bar">bar</label>
+      <input type="text" onChange={() => {console.log("I am clicked!")}} />;
+    </div>
+  );
 }
 
 export default App;
